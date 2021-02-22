17f856516e4da62bc854c15a38f670a91155ff8d
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-02-22 08:11:26 +0000

    Fixed paths in tar file

--- build_all.sh
+++ build_all.sh
@@ -6,7 +6,9 @@ for d in $DIR/master/*; do
     archive_path=out/all/mukube_master$i.tar
     mkdir -p out/all/ 
     #Pack the images and the whole i'th master folder
-    tar -cvf $archive_path $d build/tmp/
+    tar -cvf $archive_path -C $d .
+    tar -rf $archive_path -C build tmp/
+    
     i=$((i + 1))
 done
 
