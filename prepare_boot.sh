a855c63a8fec9b556c4b6462557782c7f13abf7f
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-02-23 14:56:15 +0000

    Added comment to prepare_boot script

--- prepare_boot.sh
+++ prepare_boot.sh
@@ -5,6 +5,7 @@ sudo rm -rf mukube-bootscript/
 sudo mkdir /tmp/boot_script/
 git clone https://github.com/distributed-technologies/mukube-bootscript.git
 cd mukube-bootscript/
+# TODO REMOVE THE DEV BRANCH
 git checkout bootv1
 sudo cp boot.sh /tmp/boot_script/ 
 sudo cp -r k8s-cluster-infrastructure /tmp/boot_script/ 
