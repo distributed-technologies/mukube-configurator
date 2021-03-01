962e5ca68c506d367bda64ca8ab5c0638d564cc0
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-03-01 12:48:19 +0000

    Local boot

--- scripts/prepare_boot.sh
+++ scripts/prepare_boot.sh
@@ -1,12 +1,15 @@
 # Fetch the bootscript
-sudo rm -rf /tmp/boot_script/
-sudo rm -rf mukube-bootscript/
+mkdir build/tmp/boot -p
 
-sudo mkdir /tmp/boot_script/
 git clone https://github.com/distributed-technologies/mukube-bootscript.git
 cd mukube-bootscript/
+
 # TODO REMOVE THE DEV BRANCH
 git checkout bootv1
-sudo cp boot.sh /tmp/boot_script/ 
-sudo cp -r k8s-cluster-infrastructure /tmp/boot_script/ 
+
+cp boot.sh ../build/tmp/boot/
+cp -r k8s-cluster-infrastructure ../build/tmp/boot/
+
+cd ..
+rm -rf mukube-bootscript/
 
