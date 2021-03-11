1390fa5a206b3faf38c4039453ca489a5a3cca1b
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-03-11 09:14:36 +0000

    Adding error handling to requirements.

--- scripts/pack_container_images.sh
+++ scripts/pack_container_images.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DIR=$1
+# Remake the directory to update timestamp
+rm -rf $DIR
 mkdir $DIR -p
 i=1
 while read p; do
@@ -7,3 +9,13 @@ while read p; do
   docker save --output="$DIR/$i.tar" "$p"
   i=$((i+1))
 done <requirements.txt
+
+numberOfFiles=$(ls $DIR | wc -l)
+# Counting non empty lines in requirements file
+linesInFile=$(grep -w ".*[a-z].*" -c requirements.txt)
+
+if [ $numberOfFiles != $linesInFile ]; then
+    echo "[error] Container image charts download failed. 
+          Numer of files in $DIR:$numberOfFiles does not equal the lines in image requirements:$linesInFile"
+    exit 1
+fi
