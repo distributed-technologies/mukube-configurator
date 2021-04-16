8f9a3a656841d31ac4cce005244404295a9dc1f8
skovbakke <78202835+skovbakke@users.noreply.github.com>
2021-04-16 07:15:05 +0000

    removed update script

--- scripts/yggdrasil_update_to_latest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This worsk as long as we only release a single asset.
-url_latest=$(wget -q -O - https://api.github.com/repos/distributed-technologies/yggdrasil/releases/latest | grep browser_download_url | cut -d \" -f 4 )
-
-if !(cat helm_requirements.txt 2> /dev/null | grep $url_latest 1> /dev/null) then 
-    echo "$url_latest nidhogg yggdrasil" > helm_requirements.txt
-fi
-
