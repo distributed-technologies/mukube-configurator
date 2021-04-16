13fdba64ada873658e65f29690d39e6cfac2f7ae
skovbakke <78202835+skovbakke@users.noreply.github.com>
2021-04-16 08:36:14 +0000

    reduced verbosity and added [INFO]

--- scripts/build_cluster.sh
+++ scripts/build_cluster.sh
@@ -7,10 +7,12 @@ mkdir -p $OUTDIR
 # Pack every node
 for node in $NODEDIR/master/* $NODEDIR/worker/*; do
 	archive_path=$OUTDIR/$(basename $node).tar
-	tar -cvf $archive_path -C $node .
+	echo "[INFO] Archiving $node --> $archive_path"
+	tar -cf $archive_path -C $node .
 
 	#Pack the images and helm charts for master nodes
 	if [[ $node == *"master"* ]]; then
-		tar -rvf $archive_path -C build root/helm-charts root/container-images
+		echo "	* adding helm-charts and container-images"
+		tar -rf $archive_path -C build root/helm-charts root/container-images
 	fi
 done
